e4dea78bfa57bd719379c3c441c4fe0222f4a85f
davidbt88 <ngvcho.bt88@gmail.com>
2026-06-05 16:47:19 +0700

    add secrect for env

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,38 @@
 # app/core/config.py
 import os
+from pathlib import Path
+
+def load_secret(secret_name):
+    secret_file = Path(f"/run/secrets/{secret_name}")
+
+    if not secret_file.exists():
+        return {}
+
+    result = {}
+
+    with open(secret_file, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+
+            if "=" in line:
+                k, v = line.split("=", 1)
+                result[k.strip()] = v.strip()
+
+    return result
 
 class Settings:
-    APP_NAME = os.getenv("APP_NAME", "FastAPI")
-    DB_DRIVER =  os.getenv("DB_DRIVER")
-    DB_SERVER = os.getenv("DB_SERVER")
-    DB_NAME = os.getenv("DB_NAME")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_ENCRYPT = os.getenv("DB_ENCRYPT")
-    DB_TRUST_SERVER_CERTIFICATE = os.getenv("DB_TRUST_SERVER_CERTIFICATE")
-    IMAGE_URL = os.getenv("IMAGE_URL")
+    secret_name = os.getenv("SECRET_NAME")
+    cfg = load_secret(secret_name)
+
+    APP_NAME = cfg.get("APP_NAME", "FastAPI")
+    DB_DRIVER = cfg.get("DB_DRIVER")
+    DB_SERVER = cfg.get("DB_SERVER")
+    DB_NAME = cfg.get("DB_NAME")
+    DB_USER = cfg.get("DB_USER")
+    DB_PASSWORD = cfg.get("DB_PASSWORD")
+    DB_ENCRYPT = cfg.get("DB_ENCRYPT")
+    DB_TRUST_SERVER_CERTIFICATE = cfg.get("DB_TRUST_SERVER_CERTIFICATE")
+    IMAGE_URL = cfg.get("IMAGE_URL")
     MESSAGE: str = ''
 
 settings = Settings()
